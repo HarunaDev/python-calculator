6ecbdb83a1a605ae9694d8201a561f833a48ea72
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-19 20:24:55 +0100

    created py script & added app functions in comments

---
+++ app.py
@@ -0,0 +1,5 @@
+# define functions needed for app - add, sub, multiplication & division
+# print app functions to user
+# get data from users
+# call app functions based on user selection
+# continue program until user exits the app
