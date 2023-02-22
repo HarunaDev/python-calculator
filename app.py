b759045e535dd441f8f88d94c3eaff21a38eb1b3
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-23 00:57:37 +0100

    added functions to app

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 # define functions needed for app - add, sub, multiplication & division
+
+# addition function
+def add(a, b):
+    answer = a + b 
+    print(str(a) + " + " + str(b) + " = " + str(answer))
+
+# subtraction function
+def sub(a,b):
+    answer = a - b
+    print(str(a) + " - " + str(b) + " = " + str(answer))
+
+# multiplication function
+def div(a,b):
+    answer = a * b
+    print(str(a) + " * " + str(b) + " = " + str(answer))
+
+# division function
+def multiply(a,b):
+    answer = a / b
+    print(str(a) + " / " - str(b) + " = " + str(answer))
+
 # print app functions to user
 # get data from users
 # call app functions based on user selection
