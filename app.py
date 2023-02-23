126037e6410bc8b6e9e1fcbc78a118b04755672f
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-23 01:24:47 +0100

    added code to print options & run calculations for users

--- app.py
+++ app.py
@@ -21,6 +21,40 @@ def multiply(a,b):
     print(str(a) + " / " - str(b) + " = " + str(answer))
 
 # print app functions to user
+
+print("A. Addition")
+print("B. Subtraction")
+print("C. Divsion")
+print("D. Multiplication")
+print("E. Exit") 
+
+choice = input("Input your choice: ")
+
+if choice == "a" or choice == "A":
+    print("Addition")
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    add(a,b)
+elif choice == "b" or choice == "B":
+    print("Subtraction")
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    sub(a,b)
+elif choice == "c" or choice == "C":
+    print("Division")
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    div(a,b)
+elif choice == "d" or choice == "D":
+    print("Multiply")
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    multiply(a,b)
+elif choice == "e" or choice == "E":
+    print("Program ended")
+    quit()
+
+
 # get data from users
 # call app functions based on user selection
 # continue program until user exits the app
